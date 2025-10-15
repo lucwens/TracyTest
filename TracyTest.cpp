d6eec6ddda0df825450548cce40ad06ca8f501a8
Luc Wens <luc.wens@ctechmetrology.com>
2025-10-15 17:10:39 +0200

    Integrate Tracy profiler into the project
    Added Tracy as a submodule and updated `.gitmodules` to include its repository. Modified `TracyTest.cpp` to use Tracy profiling macros (`ZoneScoped` and `FrameMark`) and replaced the basic "Hello World!" output with a loop for profiling.
    
    Updated `TracyTest.vcxproj` to include Tracy's headers and source files, added the `TRACY_ENABLE` preprocessor definition, and configured the include paths for both Debug and Release builds.
    
    Updated `TracyTest.vcxproj.filters` to include `TracyClient.cpp` in the "Source Files" filter. Linked the Tracy submodule to commit `c556831ddc6fe26d2fce01c14c97205a9dad46d5`.

--- TracyTest.cpp
+++ TracyTest.cpp
@@ -2,19 +2,19 @@
 //
 
 #include <iostream>
+#include <Tracy.hpp>
+#include <conio.h>
+#include <thread>
+#include <chrono>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    while (!_kbhit())
+    {
+    ZoneScoped;
+        std::cout << "Hello World!\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    FrameMark;
+
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
